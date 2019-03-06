a60e70a81ab095015a9aaf51b5f1ad8c176264da
JoanneYi <a914422@yahoo.com.tw>
2019-03-06 15:27:02 +0800

    Create untitled61.py

---
+++ untitled61.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 15:01:13 2019
+
+@author: User
+"""
+#%%
+for counter, item in enumerate(['apple','pear','apricot','strawberry','banana']):
+    print(counter, item)
+
+
+#%%
+j=0
+for item in ['apple','pear','apricot','strawberry','banana']:
+    print(j,item)
+    j+=1
