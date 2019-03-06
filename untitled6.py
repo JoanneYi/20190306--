d32a2f0c9f6e6d0890bf3d48a3893673e20f1ace
JoanneYi <a914422@yahoo.com.tw>
2019-03-06 15:19:10 +0800

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -5,12 +5,12 @@ Created on Wed Mar  6 15:01:13 2019
 @author: User
 """
 #%%
-for counter, item in enumerate(['apple','pear','apricot']):
+for counter, item in enumerate(['apple','pear','apricot','banana']):
     print(counter, item)
 
 
 #%%
 j=0
-for item in ['apple','pear','apricot']:
+for item in ['apple','pear','apricot','banana']:
     print(j,item)
     j+=1
